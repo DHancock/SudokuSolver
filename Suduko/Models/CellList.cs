1669a5ac0ff041d06447ccd25d247e48b1599878
DHancock <code@davidhancock.net>
2020-07-15 12:22:24 +0100

    Use two dimensional indexor
    Simplify the indexing of cells by using x , y coordinates rather than a single dimensional index.

--- Suduko/Models/CellList.cs
+++ Suduko/Models/CellList.cs
@@ -31,20 +31,32 @@ namespace Sudoku.Models
         }
 
 
+        public Cell this[int x, int y]
+        {
+            get
+            {
+                if (Rotated) 
+                    return cells[Convert(y, x)];
+
+                return cells[Convert(x, y)];
+            }
+        }
+
+
         private static int Convert(int x, int y) => x + (y * 9);
 
 
         public IEnumerable<Cell> Row(int rowIndex)
         {
             for (int x = 0; x < 9; x++)
-                yield return this[Convert(x, rowIndex)];
+                yield return this[x, rowIndex];
         }
 
 
         public IEnumerable<Cell> Column(int columnIndex)
         {
             for (int y = 0; y < 9; y++)
-                yield return this[Convert(columnIndex, y)];
+                yield return this[columnIndex, y];
         }
 
 
@@ -56,20 +68,20 @@ namespace Sudoku.Models
             if (cubex == 0) // cube is at the start of the row
             {
                 for (int x = 3; x < 9; x++)
-                    yield return this[Convert(x, rowIndex)];
+                    yield return this[x, rowIndex];
             }
             else if (cubex == 2) // cube is at the end of the row
             {
                 for (int x = 0; x < 6; x++)
-                    yield return this[Convert(x, rowIndex)];
+                    yield return this[x, rowIndex];
             }
             else
             {
                 for (int x = 0; x < 3; x++)
-                    yield return this[Convert(x, rowIndex)];
+                    yield return this[x, rowIndex];
 
                 for (int x = 6; x < 9; x++)
-                    yield return this[Convert(x, rowIndex)];
+                    yield return this[x, rowIndex];
             }
         }
 
@@ -82,20 +94,20 @@ namespace Sudoku.Models
             if (cubey == 0) // cube is at top of the column
             {
                 for (int y = 3; y < 9; y++)
-                    yield return this[Convert(columnIndex, y)];
+                    yield return this[columnIndex, y];
             }
             else if (cubey == 2) // cube is at bottom of the column
             {
                 for (int y = 0; y < 6; y++)
-                    yield return this[Convert(columnIndex, y)];
+                    yield return this[columnIndex, y];
             }
             else
             {
                 for (int y = 0; y < 3; y++)
-                    yield return this[Convert(columnIndex, y)];
+                    yield return this[columnIndex, y];
 
                 for (int y = 6; y < 9; y++)
-                    yield return this[Convert(columnIndex, y)];
+                    yield return this[columnIndex, y];
             }
         }
 
@@ -103,21 +115,23 @@ namespace Sudoku.Models
 
         public IEnumerable<Cell> CubeColumn(int cubex, int cubey, int columnIndex)
         {
-            int cubeStartIndex = (cubex * 3) + (cubey * 27);
+            int x = (cubex * 3) + columnIndex;
+            int startY = cubey * 3;
 
             for (int y = 0; y < 3; y++)
-                yield return this[cubeStartIndex + Convert(columnIndex, y)];
+                yield return this[x, startY + y];
         }
-
 
 
         public IEnumerable<Cell> CubeRow(int cubex, int cubey, int rowIndex)
         {
-            int cubeStartIndex = (cubex * 3) + (cubey * 27);
+            int y = (cubey * 3) + rowIndex;
+            int startX = cubex * 3 ;
 
             for (int x = 0; x < 3; x++)
-                yield return this[cubeStartIndex + Convert(x, rowIndex)];
+                yield return this[startX + x, y];
         }
+
 
 
         // a cube has 9 values minus one - the source cell 
