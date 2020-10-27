e07a253651592e43cb63cb72c0bdb9432a860b26
DHancock <code@davidhancock.net>
2020-10-27 14:41:51 +0000

    Improve nullable support
    1) Convert the model's CellList from containing an array of cells to a class that derives from List<Cell>. Arrays defeat nullability analysis.
    
    2) Add a not null constraint to the List<T> extension method and remove redundant null checks.
    
    3) Set project to treat warnings as errors.

--- SudokuSolver/Common/Extensions.cs
+++ SudokuSolver/Common/Extensions.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Sudoku.Common
 {
+
     internal static class Extensions
     {
 
-        public static int CountOf<T>(this List<T> list, T target)
+        public static int CountOf<T>(this List<T> list, T target) where T : notnull
         {
-            if (list is null)
-                throw new ArgumentNullException(nameof(list));
-
             int count = 0;
 
             foreach (T item in list)  // assumes the list isn't sorted
             {
-                if (item is null)
-                {
-                    if (target is null)
-                        ++count;
-                }
-                else if (item.Equals(target))
+                if (item.Equals(target))
                     ++count;
             }
 
