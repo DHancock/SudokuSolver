c1b0009b6be4df6aea8344912fc4d982677822ee
DHancock <code@davidhancock.net>
2020-11-07 14:41:25 +0000

    Revert "Add an Enum mapper generic class"
    This reverts commit a45d26749c38ba2ca86567a7590694b8cb8ee369.

--- SudokuSolver/Models/Cell.cs
+++ SudokuSolver/Models/Cell.cs
@@ -5,7 +5,7 @@ using Sudoku.Common;
 
 namespace Sudoku.Models
 {
-    [DebuggerDisplay("[{Index}] value = {Value}, origin = {Sudoku.Common.OriginsMapper.ToName(Origin)}")]
+    [DebuggerDisplay("value = {Value}, index = {Index}")]
     internal sealed class Cell : CellBase
     {
         public Cell(int index) : base(index)
