46288bbd53128fbc59859edc3ea99b36a09c7b55
DHancock <code@davidhancock.net>
2020-12-21 17:22:41 +0000

    Move the rest of the non vm code into the view
    And add an error dialog class for displaying file open and save failures.

---
+++ SudokuSolver/Views/ErrorDialog.cs
@@ -0,0 +1,19 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace Sudoku.Views
+{
+    internal static class ErrorDialog
+    {
+        public static async void Show(MetroWindow parent, string heading, string details)
+        {
+            MetroDialogSettings dialogSettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "Close",
+                ColorScheme = MetroDialogColorScheme.Accented,
+            };
+
+            await parent.ShowMessageAsync(heading, details, MessageDialogStyle.Affirmative, dialogSettings);
+        }
+    }
+}
