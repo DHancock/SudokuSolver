395eb62d20eb4e3d4ab536d1286cc012e5fc434a
David Hancock <code@davidhancock.net>
2022-08-20 16:30:27 +0100

    Update GlobalSuppressions.cs
    Ignore more chaff so I don't miss something important

--- SudokuSolver/GlobalSuppressions.cs
+++ SudokuSolver/GlobalSuppressions.cs
@@ -8,8 +8,8 @@
 [assembly: SuppressMessage("Style", "IDE0057:Use range operator")]
 [assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement")]
 [assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression")]
+[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment")]
 [assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'")]
 [assembly: SuppressMessage("Style", "IDE0180:Use tuple to swap values")]
 
 [assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
-
