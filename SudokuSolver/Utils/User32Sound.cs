97622e0e6862eebd95e81d82ee0975ae0f696375
David Hancock <code@davidhancock.net>
2022-10-27 17:24:30 +0100

    Update nuget packages

--- SudokuSolver/Utils/User32Sound.cs
+++ SudokuSolver/Utils/User32Sound.cs
@@ -1,16 +1,10 @@
 ﻿namespace Sudoku.Utils;
 
-[SuppressMessage("CodeQuality", "IDE0051:Remove unused private members")]
 internal static class User32Sound
 {
-    private const uint MB_ICONHAND = 0x0010;
-    private const uint MB_ICONQUESTION = 0x0020;
-    private const uint MB_ICONEXCLAMATION = 0x0030;
-    private const uint MB_ICONASTERISK = 0x0040;
-
     public static void PlayExclamation()
     {
-        BOOL succeeded = PInvoke.MessageBeep(MB_ICONEXCLAMATION);
-        Debug.Assert(succeeded != 0);
+        bool succeeded = PInvoke.MessageBeep(MESSAGEBOX_STYLE.MB_ICONEXCLAMATION);
+        Debug.Assert(succeeded);
     }
 }
