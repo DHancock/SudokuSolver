2a19ebbded4708f9d9f4235ab5db710a2aed73df
David Hancock <code@davidhancock.net>
2022-12-04 14:44:08 +0000

    Simplify basic content dialogs
    They don't do layout in xaml

---
+++ SudokuSolver/Views/ErrorDialog.cs
@@ -0,0 +1,18 @@
+﻿using Sudoku.Utilities;
+
+namespace Sudoku.Views
+{
+    internal sealed class ErrorDialog : ContentDialog
+    {
+        public ErrorDialog(string message, string details, XamlRoot xamlRoot, ElementTheme actualTheme) : base()
+        {
+            XamlRoot = xamlRoot;
+            RequestedTheme = actualTheme;
+            Title = App.cDisplayName;
+            PrimaryButtonText = "OK";
+            Content = $"{message}{Environment.NewLine}{Environment.NewLine}{details}";
+
+            User32Sound.PlayExclamation();
+        }
+    }
+}
