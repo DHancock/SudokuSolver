54216af31057e5dbd85455fbd6d90ab0d5ec6ad8
David Hancock <code@davidhancock.net>
2023-05-02 13:09:45 +0100

    Tie app and installer to WinAppSdk 1.3 latest

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -10,7 +10,7 @@ public static class Program
     [STAThread]
     static async Task Main(string[] args)
     {
-        if (Bootstrap.TryInitialize(FindRollForwardSdkVersion(3000), out int hresult))  // WinAppSdk 1.3 onwards
+        if (Bootstrap.TryInitialize(0x00010003, out int hresult))
         {
             try
             {
@@ -63,53 +63,5 @@ public static class Program
         string[] fileTypes = new[] { App.cFileExt };
 
         ActivationRegistrationManager.UnregisterForFileTypeActivation(fileTypes, Environment.ProcessPath);
-    }
-
-    private static uint FindRollForwardSdkVersion(uint minimumPackageMajorVersion)
-    {
-        const string cMicrosoft = "8wekyb3d8bbwe";
-
-        object lockObject = new object();
-        PackageManager packageManager = new PackageManager();
-        ProcessorArchitecture architecture = GetProcessorArchitecture();
-
-        uint latestPackageMajorVersion = minimumPackageMajorVersion;
-
-        Parallel.ForEach(packageManager.FindPackagesForUserWithPackageTypes("", PackageTypes.Main), package =>
-        {
-            if ((package.Id.Architecture == architecture) &&
-                (package.Id.Version.Major > minimumPackageMajorVersion) &&
-                string.Equals(package.Id.PublisherId, cMicrosoft, StringComparison.Ordinal) &&
-                package.Id.FullName.StartsWith("Microsoft.WinAppRuntime.DDLM.") &&
-                (package.Dependencies.Count == 1))
-            {
-                // check the DDLM package has a dependency on a framework package
-                Windows.ApplicationModel.Package dependency = package.Dependencies[0];
-
-                if (dependency.IsFramework &&
-                    dependency.Id.FullName.StartsWith("Microsoft.WindowsAppRuntime.1."))
-                {
-                    lock (lockObject)
-                    {
-                        if (latestPackageMajorVersion < dependency.Id.Version.Major)
-                            latestPackageMajorVersion = dependency.Id.Version.Major;
-                    }
-                }
-            }
-        });
-
-        return 0x00010000 + (latestPackageMajorVersion / 1000);
-    }
-
-    private static ProcessorArchitecture GetProcessorArchitecture()
-    {
-        switch (RuntimeInformation.ProcessArchitecture)
-        {
-            case Architecture.X86: return ProcessorArchitecture.X86;
-            case Architecture.X64: return ProcessorArchitecture.X64;
-            case Architecture.Arm64: return ProcessorArchitecture.Arm64;
-
-            default: return ProcessorArchitecture.Unknown;
-        }
     }
 }
