2935069026a95cc27f89ca8840fc33f995343e19
David Hancock <code@davidhancock.net>
2023-05-26 11:37:18 +0100

    Add scaffolding for new Settings window
    To be implemented...

--- SudokuSolver/Views/MainWindow.xaml.cs
+++ SudokuSolver/Views/MainWindow.xaml.cs
@@ -8,21 +8,13 @@ public enum WindowState { Normal, Minimized, Maximized }
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-internal sealed partial class MainWindow : Window
+internal sealed partial class MainWindow : WindowBase
 {
-    private const double MinWidth = 320;  
-    private const double MinHeight = 375;
-    public const double InitialWidth = 563;
-    public const double InitialHeight = 614;
     private enum Error { Success, Failure }
     private enum Status { Cancelled, Continue }
 
-    public IntPtr WindowPtr { get; }
     public WindowViewModel ViewModel { get; }
 
-    private readonly SUBCLASSPROC subClassDelegate;
-    private PointInt32 restorePosition;
-    private SizeInt32 restoreSize;
     private PrintHelper? printHelper;
     private StorageFile? sourceFile;
     private bool processingClose = false;
@@ -32,14 +24,6 @@ internal sealed partial class MainWindow : Window
     public MainWindow(StorageFile? storagefile, MainWindow? creator)
     {
         InitializeComponent();
-
-        WindowPtr = WindowNative.GetWindowHandle(this);
-
-        // sub class to set a minimum window size
-        subClassDelegate = new SUBCLASSPROC(NewSubWindowProc);
-
-        if (!PInvoke.SetWindowSubclass((HWND)WindowPtr, subClassDelegate, 0, 0))
-            throw new Win32Exception(Marshal.GetLastPInvokeError());
 
         // each window needs a local copy of the common view settings
         Settings.PerViewSettings viewSettings = Settings.Data.ViewSettings.Clone();
@@ -58,9 +42,6 @@ internal sealed partial class MainWindow : Window
             args.Cancel = true;
             await HandleWindowClosing();
         };
-
-        Activated += App.Instance.RecordWindowActivated;
-        AppWindow.Changed += AppWindow_Changed;
 
         if (AppWindowTitleBar.IsCustomizationSupported())
         {
@@ -130,76 +111,7 @@ internal sealed partial class MainWindow : Window
         };
     }
 
-    private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
-    {
-        if (WindowState == WindowState.Normal)
-        {
-            if (args.DidPositionChange)
-                restorePosition = AppWindow.Position;
 
-            if (args.DidSizeChange)
-                restoreSize = AppWindow.Size;
-        }
-    }
-
-    private LRESULT NewSubWindowProc(HWND hWnd, uint uMsg, WPARAM wParam, LPARAM lParam, nuint uIdSubclass, nuint dwRefData)
-    {
-        if (uMsg == PInvoke.WM_GETMINMAXINFO)
-        {
-            MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
-            double scaleFactor = GetScaleFactor();
-            minMaxInfo.ptMinTrackSize.X = Math.Max(ConvertToDeviceSize(MinWidth, scaleFactor), minMaxInfo.ptMinTrackSize.X);
-            minMaxInfo.ptMinTrackSize.Y = Math.Max(ConvertToDeviceSize(MinHeight, scaleFactor), minMaxInfo.ptMinTrackSize.Y);
-            Marshal.StructureToPtr(minMaxInfo, lParam, true);
-        }
-
-        return PInvoke.DefSubclassProc(hWnd, uMsg, wParam, lParam);
-    }
-
-    public WindowState WindowState
-    {
-        get
-        {
-            if (AppWindow.Presenter is OverlappedPresenter op)
-            {
-                switch (op.State)
-                {
-                    case OverlappedPresenterState.Minimized: return WindowState.Minimized;
-                    case OverlappedPresenterState.Maximized: return WindowState.Maximized;
-                    case OverlappedPresenterState.Restored: return WindowState.Normal;
-                }
-            }
-
-            return WindowState.Normal;
-        }
-
-        set
-        {
-            if (AppWindow.Presenter is OverlappedPresenter op)
-            {
-                switch (value)
-                {
-                    case WindowState.Minimized: op.Minimize(); break;
-                    case WindowState.Maximized: op.Maximize(); break;
-                    case WindowState.Normal: op.Restore(); break;
-                }
-            }
-        }
-    }
-
-    public RectInt32 RestoreBounds
-    {
-        get => new RectInt32(restorePosition.X, restorePosition.Y, restoreSize.Width, restoreSize.Height);
-    }
-
-    public static int ConvertToDeviceSize(double value, double scalefactor) => Convert.ToInt32(Math.Clamp(value * scalefactor, 0, short.MaxValue));
-
-    public double GetScaleFactor()
-    {
-        // if the xaml hasn't loaded yet, Content.XamlRoot.RasterizationScale isn't an option
-        double dpi = PInvoke.GetDpiForWindow((HWND)WindowPtr);
-        return dpi / 96.0;
-    }
 
     private async Task HandleWindowClosing()
     {
@@ -539,5 +451,10 @@ internal sealed partial class MainWindow : Window
                                 Convert.ToInt32(y * scale), 
                                 Convert.ToInt32(width * scale), 
                                 Convert.ToInt32(height * scale));
+    }
+
+    private void SettingsClickHandler(object sender, RoutedEventArgs e)
+    {
+        App.Instance.ShowSettingsWindow();
     }
 }
