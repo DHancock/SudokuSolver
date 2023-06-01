f468ebc670fad051460ad6826010722446b4e21c
David Hancock <code@davidhancock.net>
2023-06-01 08:17:41 +0100

    Add keyboard support to color picker

--- SudokuSolver/Utilities/Utils.cs
+++ SudokuSolver/Utilities/Utils.cs
@@ -7,4 +7,39 @@ internal static class Utils
         bool succeeded = PInvoke.MessageBeep(MESSAGEBOX_STYLE.MB_ICONEXCLAMATION);
         Debug.Assert(succeeded);
     }
+
+    public static int Clamp2DHorizontalIndex(int index, int total)
+    {
+        int remainder = index % total;
+
+        if (index < 0)
+            return (remainder == 0) ? 0 : total + remainder;
+
+        return remainder;
+    }
+
+    public static int Clamp2DVerticalIndex(int index, int cellsInRow, int total)
+    {
+        if (index < 0) // moving up from the top row, select the last index in the next column to the right
+            return index == -1 ? total - cellsInRow : (total + index + 1);
+
+        if (index >= total) // moving down from the bottom row, select the first index in the next column to the left
+            return index == total ? cellsInRow - 1 : (index - total - 1);
+
+        return index;
+    }
+
+    //private static int ClampVerticalIndex(int index)
+    //{
+    //    if (index < 0) // moving up from the top row, select the last cell in the next column to the right
+    //        return index == -1 ? cCellCount - cCellsInRow : (cCellCount + index + 1);
+
+    //    if (index >= cCellCount) // moving down from the bottom row, select the first cell in the next column to the left
+    //        return index == cCellCount ? cCellsInRow - 1 : (index - cCellCount - 1);
+
+    //    return index;
+    //}
+
+
+
 }
