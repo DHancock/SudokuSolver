af8568a3e269ddbbd999159a84bb44ce91a2722b
David Hancock <code@davidhancock.net>
2023-06-03 20:59:39 +0100

    Correct the up/down key movement
    Down key now moves to the last puzzle square

--- SudokuSolver/Utilities/Utils.cs
+++ SudokuSolver/Utilities/Utils.cs
@@ -20,12 +20,14 @@ internal static class Utils
 
     public static int Clamp2DVerticalIndex(int newIndex, int itemsInRow, int total)
     {
-        if (newIndex < 0) // moving up from the top row, select the last index in the next column to the right
-            return newIndex == -1 ? total - itemsInRow : (total + newIndex + 1);
+        if (newIndex < 0) // moving up from the top row, select the last index in the next column to the left
+            return newIndex == -itemsInRow ? total - 1 : (total + newIndex - 1);
 
-        if (newIndex >= total) // moving down from the bottom row, select the first index in the next column to the left
-            return newIndex == total ? itemsInRow - 1 : (newIndex - total - 1);
+        if (newIndex >= total) // moving down from the bottom row, select the first index in the next column to the right
+            return newIndex == total + itemsInRow - 1 ? 0 : newIndex - total + 1;
 
         return newIndex;
     }
+
+
 }
