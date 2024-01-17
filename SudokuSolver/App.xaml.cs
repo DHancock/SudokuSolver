e799ef6c204b91f3a0a7ea1edb2265cb4b96c937
David Hancock <code@davidhancock.net>
2024-01-17 18:52:47 +0000

    fix a string comparison message

--- SudokuSolver/App.xaml.cs
+++ SudokuSolver/App.xaml.cs
@@ -117,7 +117,7 @@ public partial class App : Application
         {
             string arg = args[index];
 
-            if (!string.IsNullOrEmpty(arg) && Path.GetExtension(arg).ToLower() == App.cFileExt && File.Exists(arg))
+            if (App.cFileExt.Equals(Path.GetExtension(arg), StringComparison.OrdinalIgnoreCase) && File.Exists(arg))
             {
                 StorageFile storgeFile = await StorageFile.GetFileFromPathAsync(arg);
                 CreateNewWindow(storgeFile);
