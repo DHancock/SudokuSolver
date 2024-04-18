e983fca0fb866dcfce5da394e90bc5bb2ade98d5
David Hancock <code@davidhancock.net>
2024-04-18 09:24:56 +0100

    Fix file type associations
    Move the code into the app OnLaunched() method. The WinAppSdk will have been initialized then, whether it's self contained or framework dependent

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -8,54 +8,24 @@ public static class Program
     private const string cAppKey = "586A28D4-3FF0-42B2-829B-5F02BBFC8352";
 
     [STAThread]
-    static void Main(string[] args)
+    static void Main()
     {
-        bool isRegister = (args.Length == 1) && string.Equals(args[0], "/register", StringComparison.Ordinal);
-        bool isUnregister = (args.Length == 1) && string.Equals(args[0], "/unregister", StringComparison.Ordinal);
+        AppInstance appInstance = AppInstance.FindOrRegisterForKey(cAppKey);
 
-        if (isRegister)
+        if (!appInstance.IsCurrent)
         {
-            RegisterFileTypeActivation();
-        }
-        else if (isUnregister)
-        {
-            UnregisterFileTypeActivation();
+            AppActivationArguments aea = AppInstance.GetCurrent().GetActivatedEventArgs();
+            RedirectActivationTo(aea, appInstance);
         }
         else
         {
-            AppInstance appInstance = AppInstance.FindOrRegisterForKey(cAppKey);
-
-            if (!appInstance.IsCurrent)
+            Application.Start((p) =>
             {
-                AppActivationArguments aea = AppInstance.GetCurrent().GetActivatedEventArgs();
-                RedirectActivationTo(aea, appInstance);
-            }
-            else
-            {
-                Application.Start((p) =>
-                {
-                    var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
-                    SynchronizationContext.SetSynchronizationContext(context);
-                    _ = new App(appInstance);
-                });
-            }
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _ = new App(appInstance);
+            });
         }
-    }
-
-    private static void RegisterFileTypeActivation()
-    {
-        string[] fileTypes = [App.cFileExt];
-        string[] verbs = ["open"];
-        string logo = $"{Environment.ProcessPath},0";
-
-        ActivationRegistrationManager.RegisterForFileTypeActivation(fileTypes, logo, App.Instance.AppDisplayName, verbs, string.Empty);
-    }
-
-    private static void UnregisterFileTypeActivation()
-    {
-        string[] fileTypes = [App.cFileExt];
-
-        ActivationRegistrationManager.UnregisterForFileTypeActivation(fileTypes, Environment.ProcessPath);
     }
 
     public static void RedirectActivationTo(AppActivationArguments args, AppInstance keyInstance)
