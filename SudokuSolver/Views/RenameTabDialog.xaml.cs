2921c27f937cdca7b01d78976c48a168294f2e56
David Hancock <code@davidhancock.net>
2025-01-06 16:05:39 +0000

    Improve pasting into new tab name text box

--- SudokuSolver/Views/RenameTabDialog.xaml.cs
+++ SudokuSolver/Views/RenameTabDialog.xaml.cs
@@ -14,7 +14,7 @@ internal sealed partial class RenameTabDialog : ContentDialog
         SecondaryButtonText = App.Instance.ResourceLoader.GetString("CancelButton");
 
         invalidFileNameChars = Path.GetInvalidFileNameChars();
-        Array.Sort(invalidFileNameChars);
+        invalidFileNameChars.AsSpan().Sort();
 
         Loaded += (s, e) =>
         {
@@ -27,7 +27,16 @@ internal sealed partial class RenameTabDialog : ContentDialog
     {
         if (args.NewText.Length > 0)
         {
-            args.Cancel = args.NewText.Any(c => Array.BinarySearch(invalidFileNameChars, c) >= 0);
+            ReadOnlySpan<char> invalidChars = invalidFileNameChars.AsSpan();
+
+            foreach (char c in args.NewText)
+            {
+                if (invalidChars.BinarySearch(c) >= 0)
+                {
+                    args.Cancel = true;
+                    break;
+                }
+            }
         }
     }
 
@@ -37,4 +46,59 @@ internal sealed partial class RenameTabDialog : ContentDialog
     }
 
     public string NewName => NewTabNameTextBox.Text.Trim();
+
+    private async void NewTabNameTextBox_Paste(object sender, TextControlPasteEventArgs e)
+    {
+        e.Handled = true; // disable the default paste action
+
+        DataPackageView view = Clipboard.GetContent();
+
+        if (view.Contains(StandardDataFormats.Text))
+        {
+            try
+            {
+                string pasteText = await view.GetTextAsync();
+                pasteText = FilterInvalidChars(pasteText);
+
+                if (pasteText.Length > 0) // mirrors behavior when renaming files in Explorer
+                {
+                    TextBox nameTextBox = (TextBox)sender;
+
+                    string left = nameTextBox.Text.Substring(0, nameTextBox.SelectionStart);
+                    string right = nameTextBox.Text.Substring(nameTextBox.SelectionStart + nameTextBox.SelectionLength);
+
+                    nameTextBox.Text = left + pasteText + right;
+                    nameTextBox.SelectionStart = left.Length + pasteText.Length;
+
+                    if (nameTextBox.Text.Length > nameTextBox.MaxLength)
+                    {
+                        int caretPos = Math.Min(nameTextBox.SelectionStart, nameTextBox.MaxLength);
+
+                        nameTextBox.Text = nameTextBox.Text.Substring(0, nameTextBox.MaxLength);
+                        nameTextBox.SelectionStart = caretPos;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    private string FilterInvalidChars(string source)
+    {
+        StringBuilder sb = new StringBuilder(source.Length);
+        ReadOnlySpan<char> invalidChars = invalidFileNameChars.AsSpan();
+
+        foreach (char c in source)
+        {
+            if (invalidChars.BinarySearch(c) < 0)
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
 }
